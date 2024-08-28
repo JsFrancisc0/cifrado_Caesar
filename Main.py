f0b7787f2885dcc0621921982cceeb172ff7b21c
JsFrancisc0 <j.saldias03@ufromail.cl>
2024-08-27 20:13:57 -0400

    jdjsyt

---
+++ Main.py
@@ -0,0 +1,10 @@
+class Main:
+    def __init__(self):
+        print("Hello, PyCharm!")
+
+    def run(self):
+        print("Running the main function.")
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
